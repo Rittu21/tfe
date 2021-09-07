498e817090f20fdacae2fd7158a31c30fc671476
Rittu21 <34350626+Rittu21@users.noreply.github.com>
2021-09-07 16:31:51 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "subscription_id" {
+    description = "subscription id"
+}
+variable "tenant_id" {
+    description = "tenant_id"
+}
+variable "client_id" {
+    description = "client_id"
+}
+variable "client_secret" {
+    description = "client_secret"
+}
