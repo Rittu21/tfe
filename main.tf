85c1bea631da5de8fb1be5a01f07f593b699ff03
Rittu21 <34350626+Rittu21@users.noreply.github.com>
2021-09-23 02:24:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,3 +55,39 @@ resource "azurerm_network_security_group" "networking" {
       finops-owner = "mno"
   }
 }
+
+resource "azurerm_network_interface" "example" {
+  name                = "test-nic"
+  location            = "EastUS"
+  resource_group_name = "test-rg"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.networking.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "test-vm"
+  resource_group_name = "test-rg"
+  location            = "EastUS"
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
