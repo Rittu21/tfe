d8b3fa33673dde9f1a706d7d62e4d187f0ad3fcb
Rittu21 <34350626+Rittu21@users.noreply.github.com>
2021-09-23 01:58:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,11 +47,10 @@ resource "azurerm_network_security_group" "networking" {
   location            = "EastUS"
   name                = "test-nsg"
   tags    = {
-      
+      component = "abc"
       product = "def"
       owner = "ghi"
       jiraproject = "jkl"
       environment = "development"
-      finops-owner = "mno"
   }
 }
