34a5c5412e2142b68e69b4e740a4bc8f116fa699
Rittu21 <34350626+Rittu21@users.noreply.github.com>
2021-09-23 02:29:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,3 +77,27 @@ resource "azurerm_network_interface" "example" {
       environment = "dev"
   }
 }
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "test-vm"
+  resource_group_name = "test-rg"
+  location            = "EastUS"
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
